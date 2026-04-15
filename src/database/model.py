b4537315634adb88d8d7f0607c4279ad8d2bffa7
mubaraizzat <mubaraizzat@gmail.com>
2026-04-15 19:15:01 +0400

    feat: add FinanceApp model with fields for user data and predictions

---
+++ src/database/model.py
@@ -0,0 +1,20 @@
+from sqlmodel import SQLModel, Field
+from .enum.FinanceAppEnum import maritalstatus , HouseOwnership
+
+
+class FinanceApp(SQLModel, table=True):
+    Id: int = Field(default=None, primary_key=True)
+    Income: float = Field(..., gt=0)
+    Age: int = Field(..., ge=18, le=100)
+    Experience: int
+    marital_status: maritalstatus
+    House_Ownership: HouseOwnership
+    Car_Ownership: str
+    Profession: str
+    CITY: str
+    STATE: str
+    CURRENT_JOB_YRS: int
+    CURRENT_HOUSE_YRS: int
+    prediction: int = Field(default=None)
+    Probability: float = Field(default=None)
+    
